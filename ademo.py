e046446722e811ca297d79d6e7fd6df8f2900866
SandipNikam <sandipnikam6082@gmail.com>
2024-06-03 16:15:55 +0530

    Add files via upload

---
+++ ademo.py
@@ -0,0 +1,29 @@
+# num = int(input("enter no="))
+# count = 0
+# if num >= 2:
+#     for i in range(1, num+1):
+#         if num %i == 0:
+#             count += 1
+#     if count ==2:
+#         print("prime number")
+#     else :
+#         print("Not prime number")
+# else:
+#     print("enter number greater than 1")
+
+# num = int(input("enter no="))
+# if num >0:
+#     if num % 2 ==0:
+#         print("enven number")
+#     else:
+#         print("odd number")
+# print("enter number greater than 0")
+
+org_num = 12321
+num =org_num
+while num!=0:
+    reverse_num=num%10
+
+
+
+
